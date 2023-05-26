e32163bf404158aea132365c4c07e024386b519e
Brooke Adlesperger-Jones <badles@live.com>
2023-05-25 21:22:49 -0500

    updated readme

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # module-six-challenge
+
 5 Day Weather Forecast
+
+## Description
+
+The goal of this project was to create a weather application. 
+
+## Installation
+
+N/A
+
+## Usage
+
+<img src="./assets/images/RM1.png"/>
+<img src="./assets/images/RM2.png"/>
+
+
+The usage of this weather application is for a user to be able to easily look up the current weather for any city. The city name, current temperature, humidity and wind will display as well as a logo for the current weather conditions. The previous searches will save after the user hits the search bar or enter key. 
+
+## Credits
+
+Referenced the following link for countdown timer: src=".https://stackoverflow.com/questions/20618355/how-to-write-a-countdown-timer-in-javascript"
+
+## License
+
+Please refer to the license in the repo
--- assets/script.js
+++ assets/script.js
@@ -1,70 +1,90 @@
 var apiKey = "47d8ef96a06e656505891bda3243fc0e";
 var apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=imperial&q=";
-var forecastUrl ="https://api.openweathermap.org/data/2.5/forecast?units=imperial&q=";
+var forecastUrl = "https://api.openweathermap.org/data/2.5/forecast?units=imperial&q=&appid=";
 
 var searchBox = document.querySelector(".form-inline input");
 var searchBtn = document.querySelector(".form-inline button");
 var weatherIcon = document.querySelector(".weather-icon");
 var previousSearchBtn = document.querySelector(".previous-search");
+var forecastResult = document.querySelector(".day-one");
 
-searchBtn.addEventListener('click', function(){
-    checkWeather();
-    previousSearch();
+// Search button function
+
+searchBtn.addEventListener("click", function () {
+  var citySearch = searchBox.value.trim();
+
+  checkWeather(citySearch);
+
+  // Save the city search value in local storage
+  localStorage.setItem("previousSearch", citySearch);
+
+  previousSearch();
 });
 
-function previousSearch () {
-    var citySearch = searchBox.value.trim();
-    var previousBtn = document.createElement("button");
-    previousBtn.classList.add("btn", "btn-outline-secondary");
-    previousBtn.textContent = citySearch;
-    previousSearchBtn.appendChild(previousBtn);
+function previousSearch() {
+  var citySearch = searchBox.value.trim();
+  var previousBtn = document.createElement("button");
+  previousBtn.classList.add("btn", "btn-outline-secondary");
+  previousBtn.textContent = citySearch;
+  previousSearchBtn.appendChild(previousBtn);
 }
-  
+
+// Get current weather forecast
+
 async function checkWeather(city) {
-    var response = await fetch(apiUrl + city + `&appid=${apiKey}`);
-    var data = await response.json();
+  var response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+  var data = await response.json();
 
-    document.querySelector(".city").innerHTML = data.name;
-    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°F";
-    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
-    document.querySelector(".wind").innerHTML = data.wind.speed + "MPH";
+  
 
-    if(data.weather[0].main == "Clouds") {
-        weatherIcon.src = "assets/images/cloudy.webp";
-    }
-    else if(data.weather[0].main == "Clear"){
-        weatherIcon.src = "assets/images/clear.png";
-    }
-    else if(data.weather[0].main == "Rain"){
-        weatherIcon.src = "assets/images/rain.png";
-    }
-    else if(data.weather[0].main == "Drizzle"){
-        weatherIcon.src = "assets/images/drizzle.png";
-    }
-    else if(data.weather[0].main == "Mist"){
-        weatherIcon.src = "assets/images/mist.png";
-    }
-    else if(data.weather[0].main == "Snow"){
-        weatherIcon.src = "assets/images/snow.png";
-    }
+  document.querySelector(".city").innerHTML = data.name;
+  document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°F";
+  document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+  document.querySelector(".wind").innerHTML = data.wind.speed + "MPH";
 
+  if (data.weather[0].main == "Clouds") {
+    weatherIcon.src = "assets/images/cloudy.webp";
+  } else if (data.weather[0].main == "Clear") {
+    weatherIcon.src = "assets/images/clear.png";
+  } else if (data.weather[0].main == "Rain") {
+    weatherIcon.src = "assets/images/rain.png";
+  } else if (data.weather[0].main == "Drizzle") {
+    weatherIcon.src = "assets/images/drizzle.png";
+  } else if (data.weather[0].main == "Mist") {
+    weatherIcon.src = "assets/images/mist.png";
+  } else if (data.weather[0].main == "Snow") {
+    weatherIcon.src = "assets/images/snow.png";
+  }
 }
 
-searchBtn.addEventListener("click", ()=>{
-    checkWeather(searchBox.value);
-})
+// Event listener for the Enter key
+searchBox.addEventListener('keydown', function(event) {
+  // Check if the pressed key is the Enter key (key code 13)
+  if (event.keyCode === 13) {
+      event.preventDefault(); // Prevent the form from submitting
+      checkWeather(searchBox.value);
+      previousSearch();
+      searchBox.value = ""; // Clear the search input
+  }
+});
 
+// Event listener for the search button
+searchBtn.addEventListener("click", function() {
+  checkWeather(searchBox.value);
+  previousSearch();
+  searchBox.value = ""; // Clear the search input
+});
 
-checkWeather();
-previousSearch();
+// Retrieve the previous search value from local storage and call checkWeather()
+var previousSearchValue = localStorage.getItem("previousSearch");
 
+if (previousSearchValue) {
+  checkWeather(previousSearchValue);
+}
 
 // Create a new Date object and format it as mm/dd/yyyy
 var today = new Date();
-var formattedDate = (today.getMonth() + 1) + '/' + today.getDate() + '/' + today.getFullYear();
+var formattedDate = today.getMonth() + 1 + "/" + today.getDate() + "/" + today.getFullYear();
 
 // Add the formatted date to an HTML element
 document.getElementById("currentDate").innerHTML = formattedDate;
-
-
-
